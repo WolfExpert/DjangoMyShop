37705ca4285ee67bf27c5ba7e3236cdff52b6ef7
WolfExpert2 <89626023+WolfExpert@users.noreply.github.com>
2024-01-16 14:03:05 +0400

    categories in header good

--- shop/models.py
+++ shop/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
-        ordering = ['name']
+        ordering = ['id']
         indexes = [
             models.Index(fields=['name']),
         ]
@@ -26,7 +26,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    image = models.ImageField(upload_to='products/', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
