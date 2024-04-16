c8c382ebfde9ce525a42f330ec5ec50e4c7851af
WolfExpert2 <89626023+WolfExpert@users.noreply.github.com>
2024-04-16 15:38:43 +0400

    detail.html(+css) with cart

--- cart/forms.py
+++ cart/forms.py
@@ -7,7 +7,9 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
                                 choices=PRODUCT_QUANTITY_CHOICES,
-                                coerce=int)
+                                coerce=int,
+                                widget=forms.Select(attrs={'class': 'quantity-selector'})
+    )
     override = forms.BooleanField(required=False,
                                   initial=False,
                                   widget=forms.HiddenInput)
